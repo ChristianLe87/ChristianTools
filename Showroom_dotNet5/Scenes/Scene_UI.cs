23e3e0dc4efd3b69a7c99a0aafe18199521d392d
Christian Lehnhoff <christianle87@icloud.com>
2021-02-06 12:48:10 +0100

    some cleaning

--- Showroom_dotNet5/Scenes/Scene_UI.cs
+++ Showroom_dotNet5/Scenes/Scene_UI.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using zTools;
 using zUI;
 
