0467ea9249ba2291b9e0cae083531e1a034e9a77
Christian Lehnhoff <christianle87@icloud.com>
2021-02-22 13:36:01 +0100

    when line is on X = 0

--- zUI/Line.cs
+++ zUI/Line.cs
@@ -28,13 +28,42 @@ namespace zUI
         {
             int amountOn_X = Math.Abs(start.X - end.X) / thickness;
             int amountOn_Y = Math.Abs(start.Y - end.Y) / thickness;
-            rectangles = new Rectangle[amountOn_X + 1];
+            rectangles = new Rectangle[Math.Max(amountOn_X, amountOn_Y) + 1];
 
             float m = Tools.MyMath.M(start.ToVector2(), end.ToVector2());
             float b = Tools.MyMath.B(start.X, start.Y, m);
 
+           
+            // When X == 0
+            if (amountOn_X == 0)
+            {
+                for (int i = 0; i < rectangles.Length; i++)
+                {
+                    Rectangle rectangle = new Rectangle();
+
+
+                    // Calculate Y
+                    if (start.Y - end.Y < 0)
+                        rectangle.Y = (start.Y + (i * thickness) - (thickness / 2));
+                    else
+                        rectangle.Y = (start.Y - (i * thickness) - (thickness / 2));
+
+
+                    // Calculate X
+                    rectangle.X = (int)(start.X - (thickness / 2));
+
+
+                    // Width and Height
+                    rectangle.Width = thickness;
+                    rectangle.Height = thickness;
+
+
+                    // Apply
+                    rectangles[i] = rectangle;
+                }
+            }
             // When bigger on X
-            if (amountOn_X > amountOn_Y)
+            else if (amountOn_X > amountOn_Y)
             {
                 for (int i = 0; i < rectangles.Length; i++)
                 {
@@ -49,7 +78,7 @@ namespace zUI
 
 
                     // Calculate Y
-                    rectangle.Y = (int)((m * rectangle.X) + b); // y = mx +b
+                    rectangle.Y = (int)((m * rectangle.X) + b) - (thickness / 2); // y = mx +b
 
 
                     // Width and Height
@@ -61,6 +90,35 @@ namespace zUI
                     rectangles[i] = rectangle;
                 }
             }
+            // When bigger on Y
+            else if (amountOn_X < amountOn_Y)
+            {
+                for (int i = 0; i < rectangles.Length; i++)
+                {
+                    Rectangle rectangle = new Rectangle();
+
+
+                    // Calculate Y
+                    if (start.Y - end.Y < 0)
+                        rectangle.Y = (start.Y + (i * thickness) - (thickness / 2));
+                    else
+                        rectangle.Y = (start.Y - (i * thickness) - (thickness / 2));
+
+
+                    // Calculate X
+                    rectangle.X = (int)((rectangle.Y - b) / m) - (thickness / 2); // x = (y-b)/m
+
+
+                    // Width and Height
+                    rectangle.Width = thickness;
+                    rectangle.Height = thickness;
+
+
+                    // Apply
+                    rectangles[i] = rectangle;
+                }
+            }
+
         }
 
         public void Update(Point? start = null, Point? end = null)
