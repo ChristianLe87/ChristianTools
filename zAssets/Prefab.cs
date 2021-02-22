058d150160ee06aa97044a9378f4a7d9fc79d3e9
Christian Lehnhoff <christianle87@icloud.com>
2021-02-22 13:56:45 +0100

    add prefab

---
+++ zAssets/Prefab.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace zAssets
+{
+    public class Prefab
+    {
+        Texture2D texture2D;
+        Point position;
+
+        public Rectangle rectangle { get => new Rectangle(x: position.X - (texture2D.Width / 2), y: position.Y - (texture2D.Height / 2), width: texture2D.Width, height: texture2D.Height); }
+        public bool isActive;
+        public string tag { get; private init; }
+
+        public Prefab(Texture2D texture2D, Point position, bool isActive = true, string tag = "")
+        {
+            this.texture2D = texture2D;
+            this.position = position;
+            this.isActive = isActive;
+            this.tag = tag;
+        }
+
+        public void Update(Point? position = null, bool? isActive= null)
+        {
+            if (position != null)
+                this.position = new Point(position.Value.X, position.Value.Y);
+
+            if (isActive != null)
+                this.isActive = isActive.Value;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture2D, rectangle, Color.White);
+        }
+    }
+}
