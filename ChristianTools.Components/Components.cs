c96ee00dbf9e87f3f3650989c2ad38f4cd5c4b64
Christian Lehnhoff <christianle87@icloud.com>
2021-12-04 15:18:49 +0100

    Implement Components component

---
+++ ChristianTools.Components/Components.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChristianTools.Components
+{
+    public class Components
+    {
+        Dictionary<string, object> components = new Dictionary<string, object>();
+
+        public T Get<T>(string key)
+        {
+            return (T)components[key];
+        }
+
+        public void Add<T>(string key, T value)
+        {
+            components.Add(key, (T)value);
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            components[key] = (T)value;
+        }
+
+        public bool Exist(string key)
+        {
+            return components.Keys.Contains(key);
+        }
+
+        public void Remove(string key)
+        {
+            components.Remove(key);
+        }
+    }
+}
