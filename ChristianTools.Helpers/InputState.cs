d91139f0523ff9bfbc4068cf55c4bec1eac93ee1
Christian Lehnhoff <christianle87@icloud.com>
2021-12-08 15:29:06 +0100

    add some ps4 control keys

--- ChristianTools.Helpers/InputState.cs
+++ ChristianTools.Helpers/InputState.cs
@@ -7,7 +7,7 @@ namespace ChristianTools.Helpers
     public class InputState
     {
         KeyboardState keyboardState = Keyboard.GetState();
-        GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+        GamePadState gamePadState = GamePad.GetState(PlayerIndex.One); // Check the device for Player One: GamePadCapabilities gamePadCapabilities = GamePad.GetCapabilities(PlayerIndex.One);
         MouseState mouseState = Mouse.GetState();
 
         // General
@@ -31,6 +31,62 @@ namespace ChristianTools.Helpers
         // Gamepad
         public bool IsGamePadButtonDown(Buttons button) => gamePadState.IsButtonDown(button);
         public bool IsGamePadButtonUp(Buttons button) => gamePadState.IsButtonUp(button);
+
+        // PS4
+        public bool PS4_Options_Down() => gamePadState.IsButtonDown(Buttons.Start);
+        public bool PS4_Share_Down() => gamePadState.IsButtonDown(Buttons.Back);
+        public bool PS4_PS_Down() => gamePadState.IsButtonDown(Buttons.BigButton);
+
+
+        // PS4_ThumbSticks
+        /*{
+            if (gamePadState.ThumbSticks.Left.X != 0f || gamePadState.ThumbSticks.Left.Y != 0f)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine($"Left Sticks X {gamePadState.ThumbSticks.Left.X}");
+                Console.WriteLine($"Left Sticks Y {gamePadState.ThumbSticks.Left.Y}");
+                Console.WriteLine("---------------------------------");
+            }
+
+            if (gamePadState.IsButtonDown(Buttons.LeftStick))
+            {
+                Console.WriteLine("Button LeftStick");
+            }
+
+            if (gamePadState.IsButtonDown(Buttons.RightStick))
+            {
+                System.Console.WriteLine("Button RightStick");
+            }
+
+            if (gamePadState.ThumbSticks.Right.X != 0f || gamePadState.ThumbSticks.Right.Y != 0f)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine($"Right Sticks X {gamePadState.ThumbSticks.Right.X}");
+                Console.WriteLine($"Right Sticks Y {gamePadState.ThumbSticks.Right.Y}");
+                Console.WriteLine("---------------------------------");
+            }
+        }*/
+
+        // PS4_R
+        public bool PS4_R1_Down() => gamePadState.IsButtonDown(Buttons.RightShoulder);
+        public bool PS4_R2_Down() => gamePadState.IsButtonDown(Buttons.RightTrigger);
+
+        // PS4_L
+        public bool PS4_L1_Down() => gamePadState.IsButtonDown(Buttons.LeftShoulder);
+        public bool PS4_L2_Down() => gamePadState.IsButtonDown(Buttons.LeftTrigger);
+
+        // PS4_buttons
+        public bool PS4_X_Down() => gamePadState.IsButtonDown(Buttons.A);
+        public bool PS4_Circle_Down() => gamePadState.IsButtonDown(Buttons.B);
+        public bool PS4_Square_Down() => gamePadState.IsButtonDown(Buttons.B);
+        public bool PS4_Triangle_Down() => gamePadState.IsButtonDown(Buttons.Y);
+
+        // PS4_DPAd
+        public bool PS4_DPadDown_Down() => gamePadState.IsButtonDown(Buttons.DPadDown);
+        public bool PS4_DPadUp_Down() => gamePadState.IsButtonDown(Buttons.DPadUp);
+        public bool PS4_DPadLeft_Down() => gamePadState.IsButtonDown(Buttons.DPadLeft);
+        public bool PS4_DPadRight_Down() => gamePadState.IsButtonDown(Buttons.DPadRight);
+
 
         // Mouse
         //public Point Mouse_Position => mouseState.Position;
