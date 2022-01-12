b35ff1cce4a40daa00026970111a821c1ae51eba
Christian Lehnhoff <christianle87@icloud.com>
2022-01-12 10:32:11 +0100

    add Transition

--- ChristianTools/Systems/Draw/Scene.cs
+++ ChristianTools/Systems/Draw/Scene.cs
@@ -9,19 +9,19 @@ namespace ChristianTools.Systems
         {
             public static void Scene(SpriteBatch spriteBatch, IScene scene)
             {
-                if (scene.UIs != null)
-                    foreach (var ui in scene.UIs)
-                        Systems.Draw.UI(spriteBatch, ui);
+                if (scene.dxSceneDrawSystem != null)
+                    scene.dxSceneDrawSystem(spriteBatch);
+
+                if (scene.map != null)
+                    scene.map.Draw(spriteBatch);
 
                 if (scene.entities != null)
                     foreach (var entity in scene.entities)
                         Systems.Draw.Entity(spriteBatch, entity);
 
-                if (scene.map != null)
-                    scene.map.Draw(spriteBatch);
-
-                if (scene.dxSceneDrawSystem != null)
-                    scene.dxSceneDrawSystem(spriteBatch);
+                if (scene.UIs != null)
+                    foreach (var ui in scene.UIs)
+                        Systems.Draw.UI(spriteBatch, ui);
             }
         }
     }
---
+++ ChristianTools/UI/Transition.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using ChristianTools.Helpers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChristianTools.UI
+{
+    public class Transition
+    {
+        /// <summary>
+        /// When darkness slowly turns into a image (when game begins)
+        /// </summary>
+		public class FadeIn : IUI
+        {
+            public Rectangle rectangle { get; private set; }
+            public string tag { get; private set; } = "";
+
+            Texture2D texture;
+            Color color;
+            byte fadeSpeed;
+
+            public bool fadeFinish { get; private set; }
+
+            public FadeIn(byte fadeSpeed = 10)
+            {
+                this.rectangle = ChristianGame.GetScene.camera.rectangle;
+
+                this.fadeSpeed = (byte)fadeSpeed;
+
+                this.color = Color.Black;
+
+                this.texture = ChristianTools.Tools.Tools.Texture.CreateColorTexture(color);
+
+                this.fadeFinish = false;
+            }
+
+            public void Update(InputState lastInputState, InputState inputState)
+            {
+                if (fadeFinish == true)
+                    return;
+
+
+                color.A -= fadeSpeed;
+                texture = ChristianTools.Tools.Tools.Texture.CreateColorTexture(color);
+
+                if (color.A <= fadeSpeed)
+                    fadeFinish = true;
+            }
+
+            public void Draw(SpriteBatch spriteBatch)
+            {
+                if (color.A <= fadeSpeed)
+                    return;
+
+                spriteBatch.Draw(
+                    texture,
+                    new Rectangle(
+                        new Point(
+                            ChristianGame.GetScene.camera.rectangle.X,
+                            ChristianGame.GetScene.camera.rectangle.Y
+                        ),
+                        new Point(
+                            ChristianGame.viewport.Width,
+                            ChristianGame.viewport.Height
+                        )
+                    ),
+                    Color.White
+                );
+
+            }
+        }
+
+        /// <summary>
+        /// When an image slowly turns into darkness (when film ends)
+        /// </summary>
+		public class FadeOut : IUI
+        {
+            public Rectangle rectangle { get; private set; }
+            public string tag { get; private set; } = "";
+
+            Texture2D texture;
+            Color color;
+            byte fadeSpeed;
+
+            public bool fadeFinish { get; private set; }
+
+            public FadeOut(byte fadeSpeed = 10)
+            {
+                this.rectangle = ChristianGame.GetScene.camera.rectangle;
+
+                this.fadeSpeed = (byte)fadeSpeed;
+
+                this.color = Color.Black;
+                this.color.A = 0;
+
+                this.texture = ChristianTools.Tools.Tools.Texture.CreateColorTexture(color);
+
+                this.fadeFinish = false;
+            }
+
+            public void Update(InputState lastInputState, InputState inputState)
+            {
+                if (fadeFinish == true)
+                    return;
+
+
+                color.A += fadeSpeed;
+                texture = ChristianTools.Tools.Tools.Texture.CreateColorTexture(color);
+
+                if (color.A > (byte.MaxValue - fadeSpeed))
+                    fadeFinish = true;
+            }
+
+            public void Draw(SpriteBatch spriteBatch)
+            {
+                spriteBatch.Draw(
+                    texture,
+                    new Rectangle(
+                        new Point(
+                            ChristianGame.GetScene.camera.rectangle.X,
+                            ChristianGame.GetScene.camera.rectangle.Y
+                        ),
+                        new Point(
+                            ChristianGame.viewport.Width,
+                            ChristianGame.viewport.Height
+                        )
+                    ),
+                    Color.White
+                );
+            }
+        }
+    }
+}
