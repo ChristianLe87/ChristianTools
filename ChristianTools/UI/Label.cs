ee2f00622bb303d539b5f9454f96c9ffa6dab791
ChristianLe87 <christianle87@icloud.com>
2025-03-16 16:12:26 +0100

    Fix label

--- ChristianTools/UI/Label.cs
+++ ChristianTools/UI/Label.cs
@@ -4,14 +4,36 @@ namespace ChristianTools.UI
 	{
 		public DxCustomUpdateSystem dxCustomUpdateSystem { get; set; }
 		public DxCustomDrawSystem dxCustomDrawSystem { get; set; }
+
+		private Rectangle originalRectangle { get; }
+		private Rectangle scaledRectangle
+		{
+			get
+			{
+				if (UI_Position == Alignment.Null)
+				{
+					Rectangle rectangle = originalRectangle;
+					rectangle.X *= scaleFactor;
+					rectangle.Y *= scaleFactor;
+					rectangle.Width *= scaleFactor;
+					rectangle.Height *= scaleFactor;
+
+					return Helpers.MyRectangle.GetRectangleBaseOnCanvasPosition(UI_Position, rectangle, margin * scaleFactor);
+				}
+				else
+				{
+					return Helpers.MyRectangle.GetRectangleBaseOnCanvasPosition(UI_Position, originalRectangle.Width * scaleFactor, originalRectangle.Height * scaleFactor, margin * scaleFactor);
+				}
+			}
+		}
+
+
 		public bool isActive { get; set; }
 		public string tag { get; private set; }
 
 		private Texture2D defaultTexture;
-		private Rectangle rectangle;
 
 		public string text;
-		private Point textPosition;
 
 		private Alignment textAlignment = Alignment.Null;
 		private Alignment UI_Position = Alignment.Null;
@@ -22,64 +44,61 @@ namespace ChristianTools.UI
 
 		public Label(string text, Alignment textAlignment, Alignment UI_Position, int Width, int Height, int margin = 0, Texture2D texture = null, string tag = "")
 		{
-			Rectangle rec = Helpers.MyRectangle.GetRectangleBaseOnCanvasPosition(UI_Position, Width, Height, margin);
-
-			Init(text: text, textAlignment: textAlignment, texture: texture, rectangle: rec, margin: margin, UI_Position: UI_Position, tag: tag);
+			this.originalRectangle = Helpers.MyRectangle.GetRectangleBaseOnCanvasPosition(UI_Position, Width, Height, margin);
+			Init(text: text, textAlignment: textAlignment, texture: texture, margin: margin, UI_Position: UI_Position, tag: tag);
 		}
 
 		public Label(string text, Alignment textAlignment, Rectangle rectangle, Texture2D texture = null, string tag = "")
 		{
-			Init(text: text, textAlignment: textAlignment, texture: texture, rectangle: rectangle, margin: margin, tag: tag);
+			this.originalRectangle = rectangle;
+			Init(text: text, textAlignment: textAlignment, texture: texture, margin: margin, tag: tag);
 		}
 
-		private void Init(string text, Alignment textAlignment, Texture2D texture, Rectangle rectangle, string tag, int margin = 0, Alignment UI_Position = Alignment.Null)
+		private void Init(string text, Alignment textAlignment, Texture2D texture, string tag, int margin = 0, Alignment UI_Position = Alignment.Null)
 		{
 			this.tag = tag;
 			this.text = text;
 			this.textAlignment = textAlignment;
 			this.UI_Position = UI_Position;
 			this.margin = margin;
-
-			this.rectangle = rectangle;
 			this.defaultTexture = texture;
 			this.dxCustomDrawSystem = (SpriteBatch spriteBatch) => DrawSystem(spriteBatch);
-
 			this.isActive = true;
-			this.textPosition = GetTextPosition(spriteFont);
 		}
 
 		private void DrawSystem(SpriteBatch spriteBatch)
 		{
-			if (defaultTexture != null)
-				spriteBatch.Draw(defaultTexture, rectangle, Color.White);
+			if (defaultTexture != null) spriteBatch.Draw(defaultTexture, scaledRectangle, Color.White);
 
-			spriteBatch.DrawString(this.spriteFont, text, textPosition.ToVector2(), Color.White);
+			Vector2 position = GetTextPosition(this.spriteFont).ToVector2();
+			spriteBatch.DrawString(this.spriteFont, text, position, Color.White);
 		}
 
-		public void UpdateRectangle(Rectangle rectangle)
+		/*public void UpdateRectangle(Rectangle rectangle)
 		{
-			this.rectangle = rectangle;
-			this.textPosition = GetTextPosition(this.spriteFont);
-		}
+			//this.rectangle = rectangle;
+			//this.textPosition = GetTextPosition(this.spriteFont);
+		}*/
 		
 		
 		public void UpdateOnGameWindowSizeChangeEvent()
 		{
-			if (UI_Position != Alignment.Null)
-				this.rectangle = Helpers.MyRectangle.GetRectangleBaseOnCanvasPosition(this.UI_Position, rectangle.Width, rectangle.Height, this.margin);
+			//if (UI_Position != Alignment.Null) this.rectangle = Helpers.MyRectangle.GetRectangleBaseOnCanvasPosition(this.UI_Position, rectangle.Width, rectangle.Height, this.margin);
 
-			this.textPosition = GetTextPosition(this.spriteFont);
+			//this.textPosition = GetTextPosition(this.spriteFont);
 		}
 
 		private Point GetTextPosition(SpriteFont spriteFont)
 		{
-			int PosLeft_X = rectangle.X;
-			int PosCenter_X = (rectangle.Width / 2) + (rectangle.X) - ((int)spriteFont.MeasureString(text).X / 2);
-			int PosRight_X = rectangle.X + rectangle.Width - (int)spriteFont.MeasureString(text).X;
+			//Rectangle rectangle = scaledRectangle;
 
-			int PosTop_Y = rectangle.Y;
-			int PosMiddle_Y = rectangle.Center.Y - (((int)spriteFont.MeasureString(text).Y) / 2);
-			int PosDown_Y = rectangle.Y + rectangle.Height - ((int)spriteFont.MeasureString(text).Y);
+			int PosLeft_X = scaledRectangle.X;
+			int PosCenter_X = (scaledRectangle.Width / 2) + (scaledRectangle.X) - ((int)spriteFont.MeasureString(text).X / 2);
+			int PosRight_X = scaledRectangle.X + scaledRectangle.Width - (int)spriteFont.MeasureString(text).X;
+
+			int PosTop_Y = scaledRectangle.Y;
+			int PosMiddle_Y = scaledRectangle.Center.Y - (((int)spriteFont.MeasureString(text).Y) / 2);
+			int PosDown_Y = scaledRectangle.Y + scaledRectangle.Height - ((int)spriteFont.MeasureString(text).Y);
 
 			return textAlignment switch
 			{
